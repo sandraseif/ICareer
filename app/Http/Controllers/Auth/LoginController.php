5e51158c487b5c7cb3b37ff37c23c0c21e5d4aa2
Sandra Seif <sandra@unpluggedweb.com>
2021-04-14 10:26:46 +0200

    Change the redirect of the login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/mymovies';
 
     /**
      * Create a new controller instance.
